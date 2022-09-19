9faed8d88eb3510f1827116eb6bc102921b4b819
Miki Zenebe <mikiyaszenebe10@gmail.com>
2022-09-19 12:46:51 -0700

    updated

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-function Options() {
-  return <div>Options</div>;
+function Options({ children }) {
+  return (
+    <div>
+      Options
+      {children}
+    </div>
+  );
 }
 
 export default Options;
+
+/*Childern render it's passed into that component
+in this case we will pass notificatons*/
