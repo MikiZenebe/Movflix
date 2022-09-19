b842ce611c04ddee864819dd3da18189dd4fcc2e
Miki Zenebe <mikiyaszenebe10@gmail.com>
2022-09-19 09:03:00 -0700

    updated

--- server/index.js
+++ server/index.js
@@ -17,4 +17,20 @@ app.get("/", (req, res) => {
   res.send("Hello from Server");
 });
 
+//Socket io setup
+io.on("connection", (socket) => {
+  socket.emit("me", socket.io);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callended");
+  });
+
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+  });
+
+  socket.on("answercall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal);
+  });
+});
 http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
