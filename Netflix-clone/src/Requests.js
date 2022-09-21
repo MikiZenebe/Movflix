f01535b5dfe1ea6aff93586c7d516a5a47d5806e
Miki Zenebe <mikiyaszenebe10@gmail.com>
2022-09-21 10:09:16 -0700

    updated

--- Netflix-clone/src/Requests.js
+++ Netflix-clone/src/Requests.js
@@ -1,3 +1,11 @@
 const API = "7df0012a83107f367e7d00f3973c72c2";
 
-const requests = {};
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${API}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${API}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${API}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${API}&language=en-US&page=1`,
+};
+
+export default requests;
--- Netflix-clone/src/components/Main.jsx
+++ Netflix-clone/src/components/Main.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import requests from "../Requests";
 
 function Main() {
-  return <div>Main</div>;
+  const [movies, setMovies] = useState([]);
+
+  //Pick random movie to cover image
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
+  //Fetch movie data with axios
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((res) => {
+      setMovies(res.data.results);
+    });
+  }, []);
+  console.log(movie);
+  return (
+    <div className="w-full h-[550px] text-white">
+      <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+      <div className="w-full h-full">
+        <img
+          className="w-full h-full object-cover"
+          src={`http://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          alt={movie?.title}
+        />
+
+        <div>
+          <button className="border border-gray-300 py-2 px-5 bg-white text-black">
+            Play
+          </button>
+          <button className="border border-gray-300 px-5 py-2 ml-4">
+            Watch Later
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
