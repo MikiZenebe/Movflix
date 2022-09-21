d4a80a0bf70b73632a4f1d0e528c40fb60eea578
Miki Zenebe <mikiyaszenebe10@gmail.com>
2022-09-21 04:32:21 -0700

    updated

---
+++ Netflix-clone/src/Requests.js
@@ -0,0 +1,3 @@
+const API = "7df0012a83107f367e7d00f3973c72c2";
+
+const requests = {};
---
+++ Netflix-clone/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="flex justify-between p-4 z-[100] absolute w-full">
+      <h1 className="text-yellow-300 text-4xl font-bold cursor-pointer">
+        MOVFLIX
+      </h1>
+
+      <div>
+        <button className="text-white pr-4 font-bold">Sign In</button>
+        <button className="bg-yellow-300 px-6 py-2 rounded cursor-pointer font-bold">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
