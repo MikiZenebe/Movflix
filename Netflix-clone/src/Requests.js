cb0ed37f2b11668c20c4a0b06588f07b8ee879b5
Miki Zenebe <mikiyaszenebe10@gmail.com>
2022-09-22 03:51:28 -0700

    updated

--- Netflix-clone/src/Requests.js
+++ Netflix-clone/src/Requests.js
@@ -1,11 +1,15 @@
 const API = "7df0012a83107f367e7d00f3973c72c2";
 
 const requests = {
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${API}&language=en-US&page=1`,
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${API}&language=en-US&page=1`,
   requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API}&language=en-US&page=1`,
   requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${API}&language=en-US&page=2`,
+  requestActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API}&with_genres=28`,
+  requestComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API}&with_genres=35`,
+  requestRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API}&with_genres=10749`,
   requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${API}&language=en-US&query=horror&page=1&include_adult=false`,
-  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${API}&language=en-US&page=1`,
+  requestDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key=${API}&with_genres=99`,
 };
 
 export default requests;
--- Netflix-clone/src/pages/Home.jsx
+++ Netflix-clone/src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import requests from "../Requests";
+
+import Row from "../components/Row";
 import Main from "../components/Main";
 
 function Home() {
   return (
-    <div>
+    <>
       <Main />
-    </div>
+      <Row title="UpComing" fetchData={requests.requestUpcoming} />
+      <Row title="Popular" fetchData={requests.requestPopular} />
+      <Row title="Top Rated" fetchData={requests.requestTopRated} />
+      <Row title="Trending" fetchData={requests.requestTrending} />
+      <Row title="Action" fetchData={requests.requestActionMovies} />
+      <Row title="Comedy" fetchData={requests.requestComedyMovies} />
+      <Row title="Romance" fetchData={requests.requestRomanceMovies} />
+      <Row title="Horror" fetchData={requests.requestHorror} />
+      <Row title="Documentaries" fetchData={requests.requestDocumentaries} />
+    </>
   );
 }
 
