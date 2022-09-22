7a8973544bdd6a9fabb9480b804f34115f48ea87
Miki Zenebe <mikiyaszenebe10@gmail.com>
2022-09-22 09:43:26 -0700

    updated

--- Netflix-clone/src/components/Row.jsx
+++ Netflix-clone/src/components/Row.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
+import Movie from "./Movie";
 
 function Row({ title, fetchData }) {
   const [movies, setMovies] = useState([]);
@@ -13,24 +16,23 @@ function Row({ title, fetchData }) {
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={"slider"}>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          size={40}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => (
-            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-              <img
-                className="w-full h-auto block"
-                src={`http://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
-                alt={item?.title}
-              />
-
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-                  {item?.title}
-                </p>
-              </div>
-            </div>
+            <Movie key={id} item={item} />
           ))}
         </div>
+        <MdChevronRight
+          size={40}
+          className="bg-white right-0 rounded-full  absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
       </div>
     </>
   );
