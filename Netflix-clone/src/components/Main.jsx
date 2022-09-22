d97e5ca144dfc9f8506123e8afa9167c3451dcab
Miki Zenebe <mikiyaszenebe10@gmail.com>
2022-09-22 02:51:15 -0700

    updated

--- Netflix-clone/src/components/Main.jsx
+++ Netflix-clone/src/components/Main.jsx
@@ -14,7 +14,16 @@ function Main() {
       setMovies(res.data.results);
     });
   }, []);
-  console.log(movie);
+
+  //Make three dots or read more
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
+
   return (
     <div className="w-full h-[550px] text-white">
       <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
@@ -25,13 +34,21 @@ function Main() {
           alt={movie?.title}
         />
 
-        <div>
-          <button className="border border-gray-300 py-2 px-5 bg-white text-black">
-            Play
-          </button>
-          <button className="border border-gray-300 px-5 py-2 ml-4">
-            Watch Later
-          </button>
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+          <div className="my-4">
+            <button className="border border-yellow-300 py-2 px-5 bg-yellow-300 text-black ">
+              Play
+            </button>
+            <button className="border border-yellow-300 px-5 py-2 ml-4">
+              Watch Later
+            </button>
+          </div>
+
+          <p className="text-gray-400">Released: {movie?.release_date}</p>
+          <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+            {truncateString(movie?.overview, 200)}
+          </p>
         </div>
       </div>
     </div>
